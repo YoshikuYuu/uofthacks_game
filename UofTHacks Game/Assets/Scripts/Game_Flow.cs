4e404916cff5e5b54d39158d1deedd8ba912ad3f
CloudyKrypton <janetfu04@gmail.com>
2024-01-28 05:48:10 -0500

    m

---
+++ UofTHacks Game/Assets/Scripts/Game_Flow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Flow : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // UI_InputWindow.Show_Static("testing", "1234567890", (string inputText) => {}, () => {});
+        // Debug.Log("run btn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- UofTHacks Game/Assets/Scripts/Program.cs
+++ UofTHacks Game/Assets/Scripts/Program.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
-
+using UnityEngine.UI;
+using CodeMonkey;
+using CodeMonkey.Utils;
+using TMPro;
 public class Program : MonoBehaviour
 {
-
     public class PlayerCodeBlock {
         // A block of code that can be executed, with head being the first line
         //and all other lines being nested as code blocks
@@ -112,6 +113,7 @@ public class Program : MonoBehaviour
         }
         public void Execute(List<PlayerCodeBlock> nested) {
             target.TakeDamage(player.GetAttack());
+
         }
     }
 
@@ -249,14 +251,11 @@ public class Program : MonoBehaviour
             {"hydra.get_heads()", () => hydra},
             {"heads", () => hydra}
         };
-        if (false) {
-            ExecuteMain();
-        }
     }
 
-    void ExecuteMain()
+    void ExecuteMain(string[] codeLines)
     {
-        playerCodeComponent = null;
+        playerCodeComponent = codeLines;
         // Split code at newlines, trim each line, add appropriate indentaion
         // If player indents code for no reason, unindent it
         // Remove all empty lines
